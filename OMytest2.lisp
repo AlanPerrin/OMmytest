3bf5b1b6fd285ef198ffc111306ccf4cd28be8ac
Alan Barclay <alan.perrin.barclay@gmail.com>
2020-06-23 12:17:44 +0100

    I added some very cool functions. Super-cool-fuction, Mytest, and Mytest1

--- OMytest2.lisp
+++ OMytest2.lisp
@@ -1,3 +1,16 @@
 ;;========================================
 ;;                OM Test
 ;;========================================
+(in-package :om)
+
+
+(compile&load (om-relative-path '("sources") "OMMyTest";;;Name of source code file
+				))
+
+(om::fill-library '(("My Cool Test" ;what is this library called
+		     nil nil ( Super-Cool-Function;function name to used
+			      )
+		     nil)
+		    ("Other Cool Test" nil nil (Mytest)nil)
+		    ("Yet more text" nil nil (MyTest1)nil)
+		    ))
--- sources/OMMyTest.lisp
+++ sources/OMMyTest.lisp
@@ -1,1 +1,37 @@
+;========================================
+;---------------OM Cool lib--------------
+;========================================
+(in-package :om)
+
 ;;Here is where the functions go.
+(defmethod! Super-Cool-Function ((number1 number) (number2 number));in here go the arguments
+    :intitvals '(1 '2)
+    :indoc '("Does a really cool thing")
+    :icon ;style over substance 
+    :doc "My Super-Cool-function is better than yours"
+    
+    (do-super-cool-function number1 number2)
+    )
+
+
+(defun do-super-cool-function (x y)
+  (format nil "In the first inlet we have ~a and in the second we have ~a" x y)
+  )
+
+(defmethod! Mytest ((x integer))
+  :initvals '(1)
+  :indoc '("asdf")
+  :icon ;number 
+  :doc "tell me something "
+  
+  (princ x)
+  
+    )
+(defmethod! MyTest1 ((y string))
+  :initvals '("some")
+  :indoc '("some more text")
+  :icon ;number
+  :doc "more some more text"
+  
+  (concatenate 'string y " sorry could you repeat that!")
+  )
